dbd84185eea9c97b77f4dcc59e30dbff67ed8128
Yan <marynetsyy@gmail.com>
2026-09-25 17:13:17 +0300

    solution

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -24,7 +24,6 @@ SECRET_KEY = (
     "django-insecure-8ovil3xu6=eaoqd#-#&ricv159p0pypoh5_lgm*)-dfcjqe=yc"
 )
 
-# SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -107,6 +106,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTH_USER_MODEL = "taxi.Driver"
+
+LOGIN_REDIRECT_URL = "/"
+
+LOGIN_URL = "/login/"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
